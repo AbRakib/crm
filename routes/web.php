a41613b67ecfb6e1c0946c69e40536816e455c66
AbRakib <abrakib.cse@gmail.com>
2024-09-04 22:34:48 +0600

    new dashboard add and login dashboard set

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+Route::get('/dashboard', function () {
+    return view('home');
+})->name('admin.dashboard');
+
+Route::get('/show', function () {
+    return view('show');
 });
